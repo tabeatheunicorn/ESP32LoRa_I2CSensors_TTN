b0d69ebe0c4aa6e197167681db57a931bc9488ca
Tabea <tabearoethe@gmail.com>
2020-02-25 09:10:38 +0100

    Added blank project

---
+++ FirstSteps/SendingData.ino
@@ -0,0 +1,18 @@
+/*****************************************************
+ * Written by Tabea Röthemeyer(@tabeatheunicorn) 2020 
+ * 
+ ****************************************************/
+
+#include <lmic.h>
+#include <hal/hal.h>
+#include <SPI.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
